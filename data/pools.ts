b96bc87fa6938378363b6776ef0bbb6bf1face76
randseas <alchcraft24@gmail.com>
2025-01-26 14:34:05 +0300

    beatcoin

--- data/pools.ts
+++ data/pools.ts
@@ -6,7 +6,44 @@ export const pools: Pool[] = [
     network: "solana",
     pair: {
       tokenA: "SOL_native",
-      tokenB: "USDC_usdc",
+      tokenB: "USDC_EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+    },
+    totalLiquidity: 0,
+    reserves: {
+      tokenAReserve: 0,
+      tokenBReserve: 0,
+    },
+    lowerPrice: 254.51,
+    upperPrice: 254.56,
+    currentPrice: 254.55,
+    feeRate: 0,
+    volume: 0,
+    createdAt: Date.now(),
+    graph: [
+      {
+        time: 1736177230,
+        open: 0.15,
+        high: 0.17,
+        low: 0.14,
+        close: 0.16,
+        volume: 12,
+      },
+      {
+        time: 1736177290,
+        open: 0.16,
+        high: 0.18,
+        low: 0.15,
+        close: 0.17,
+        volume: 16,
+      },
+    ],
+  },
+  {
+    id: "fr1cx9r-4emfxc-p5xt3qz",
+    network: "solana",
+    pair: {
+      tokenA: "WSOL_So11111111111111111111111111111111111111112",
+      tokenB: "USDC_EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
     },
     totalLiquidity: 0,
     reserves: {
